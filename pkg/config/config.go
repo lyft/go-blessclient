f9d1ded06e5c65540044d8eb64315f1650201959
Vivian Ho <vho@lyft.com>
2018-11-14 10:48:45 -0800

    allow function version to be set

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -87,6 +87,8 @@ type LambdaConfig struct {
 	RoleARN *string `yaml:"role_arn,omitempty"`
 	// Bless lambda function name
 	FunctionName string `yaml:"function_name"`
+	// Bless lambda function version
+	FunctionVersion *string `yaml:"function_version,omitempty"`
 	// bless lambda regions
 	Regions []Region `yaml:"regions,omitempty"`
 }
